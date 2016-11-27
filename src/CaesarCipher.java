03ffffff5980141926db15c4bcc8d42258b5cf0b
Miranda McClellan <miranda.mcclellan14@gmail.com>
2016-11-27 23:48:26 +0000

    Creating console interface

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -2,13 +2,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
-public class Caesar {
-	private static final List<Character> alphabet = new ArrayList<Character>(Arrays.asList('a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w', 'x','y','z'));
+public class CaesarCipher {
+	private static final List<Character> alphabet = new ArrayList<Character>(Arrays.asList('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w', 'x','y','z'));
 	private final int offset;
 	
-	public Caesar(int offset){
+	public CaesarCipher(int offset){
 		int scaled = (offset % alphabet.size());
 		if(scaled < 0){
 			this.offset = alphabet.size() + scaled;
@@ -32,9 +33,11 @@ public class Caesar {
 	public String encode(String message){
 		String encoding = "";
 		message = message.toLowerCase();
-		for(int i = 0; i < message.length(); i++)
-			encoding += "" + shift(message.charAt(i));
-		
+		for(int i = 0; i < message.length(); i++){
+			char c = message.charAt(i);
+			if(c >= 'a' && c <= 'z')
+				encoding += "" + shift(c);
+		}
 		return encoding;
 	}
 	
@@ -50,10 +53,18 @@ public class Caesar {
 		return encoding;
 	}
 	
-	public static void main(String[] args) {
-		Caesar cae = new Caesar(4);
-		String en = cae.encode("Hello");
-		System.out.println(en);
-		System.out.println(cae.decode(en));
+	public static void main(String[] args){
+		System.out.print("Please choose a shift for the cipher: ");
+		Scanner in = new Scanner(System.in);
+		int shift = Integer.parseInt(in.nextLine());
+		CaesarCipher cipher = new CaesarCipher(shift);
+		
+		while(true){
+			System.out.print("Please enter new message: ");
+			String input = in.nextLine();
+			String encoding = cipher.encode(input);
+			System.out.println(encoding);
+			System.out.println(cipher.decode(encoding));
+		}
 	}
 }
